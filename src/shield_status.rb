1c6ee1c005759355bf6b886143a955e9610bc3f0
Abel Martin <abel.martin@gmail.com>
2012-12-24 00:46:27 -0500

    Decrementing shield strength changes the color to red.

--- src/shield_status.rb
+++ src/shield_status.rb
@@ -6,15 +6,19 @@ class ShieldStatusView < ActorView
       (target.screen.width/2) - 120,
       target.screen.height - 150,
       z,
+      1,
+      1,
+      target.convert_color(actor.color)
     )
   end
 end
 
 class ShieldStatus < Actor
-  attr_reader :strength
+  attr_reader :strength, :color
 
   def setup
     reset_strength
+    @color = [255,255,255,255]
     input_manager.reg :down, KbSpace do
       self - 15
       #play_sound :laser
@@ -23,6 +27,7 @@ class ShieldStatus < Actor
 
   def -(amount)
     @strength -= amount
+    @color = [255, @color[1]-50, @color[2]-50, 255 ]
     @strength = 0 if @strength < 0
   end
 
